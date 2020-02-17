fda6bd288420fb594f07bcf43c3b0e37f4543382
Marc van Zee <marcvanzee@google.com>
2020-02-17 10:37:24 +0100

    Changes

--- generate_templates.py
+++ generate_templates.py
@@ -8,26 +8,38 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string('repo_path', '.', 'Relative path to the Github repository.')
 
+flags.DEFINE_bool('strip_copyright_from_code', True, 'If true, removes copyright header from code.')
+
 # TODO(marcvanzee): Currently this only works for README.md. Think of a more 
 # general solution.
 
-def wrap_in_code(text, type='py'):
-  return [f'```{type}'] + text.split('\n') + ['```']
+def wrap_in_code(lines, lang):
+  return [f'```{lang}'] + lines + ['```']
+
 
 def insert_branch_diff_fn(target_branch, source_branch='prerelease'):
   git = Repo(FLAGS.repo_path).git
   diff_text = git.diff(source_branch, target_branch, '--', ':(exclude)*.md')
-  return wrap_in_code(diff_text, 'diff')
+  return wrap_in_code(diff_text.split('\n'), 'diff')
 
 
 def insert_py_code_fn(file_path, lang='py'):
-  lines = open(file_path, encoding='utf8').readlines()
-  return wrap_in_code(lines)
+  lines = open(file_path, encoding='utf8').read().splitlines()
+  if FLAGS.strip_copyright_from_code:
+    while len(lines) and lines[0].startswith('#'):
+      lines = lines[1:]
+  # Strips empty lines from start and aned.
+  while len(lines) and not lines[0].strip():
+    lines = lines[1:]
+  while len(lines) and not lines[-1].strip():
+    lines = lines[:-1]
+  
+  return wrap_in_code(lines, lang=lang)
 
 
 operations = {
   '@insert_branch_diff': insert_branch_diff_fn,
-  '@insert_py_code': insert_py_code_fn
+  '@insert_code': insert_py_code_fn
 }
 
 def generate_template(input_path, output_path):
@@ -42,7 +54,6 @@ def generate_template(input_path, output_path):
   with open(output_path, 'w', encoding='utf8') as output_f:
     output_f.write('\n'.join(output_lines))
 
-  
 
 def main(argv):
   if len(argv) > 1:
