eef65d5dfc533b17a28be1c75e9a9586710432fe
Marc van Zee <marcvanzee@google.com>
2020-02-17 16:59:56 +0100

    test

--- rebase_all_howtos.sh
+++ rebase_all_howtos.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
-# This script rebases all HOWTO branches to the main branch. It shoul be ran
-# before generating the Markdown files, to avoid unrelated changes in the main
-# branch to show up in the HOWTO diffs.
+# Rebases all HOWTO branches to the main branch. It should be ran before 
+# generating the Markdown files, to avoid unrelated changes in the main branch 
+# to show up in the HOWTO diffs.
 
 local main_branch="prerelease"
 local howto_prefix="howto-"
 
 git checkout "${main_branch}"
-
 for howto_branch in $(git branch | grep "${howto_prefix}"); do
   git rebase "${main_branch}" "${howto_branch}"
   git merge "${howto_branch}"
